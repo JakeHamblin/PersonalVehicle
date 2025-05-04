eff29ab9595213276d74c89de67d12e39aa6cfc1
Jake Hamblin <jake@jakehamblin.com>
2025-05-03 23:04:49 -0400

    init

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+author 'Jake Hamblin <jake@jakehamblin.com>'
+description 'Limit access to addon vehicles in game to specific ACE permission groups'
+version '1.0.0'
+
+client_scripts {
+    'client.lua',
+}
+
+server_scripts {
+	'server.lua',
+}
