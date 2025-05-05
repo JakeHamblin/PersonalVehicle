f4e3fc4c72859c229e481fde7f66d80c65f0fa0f
Jake Hamblin <jake@jakehamblin.com>
2025-05-04 23:49:21 -0400

    Modified comment

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,13 +2,19 @@ fx_version 'cerulean'
 games {'gta5'}
 
 author 'Jake Hamblin <jake@jakehamblin.com>'
-description 'Limit access to addon vehicles in game to specific ACE permission groups'
+description 'Limit access to addon vehicles in game to specific Discord users'
 version '1.0.0'
 
 client_scripts {
+    '@NativeUI/NativeUI.lua',
+    'config.lua',
     'client.lua',
 }
 
 server_scripts {
-	'server.lua',
+    'server.lua',
+}
+
+files {
+    'vehicles.json',
 }
